7430706627db9f8fa0dc18060e487c56d6145eb1
AbijaK <abijamk@gmail.com>
2022-12-17 13:37:58 +0100

    update script: break & reset

--- js/script.js
+++ js/script.js
@@ -1,76 +1,79 @@
-// Variables
-let progressBar = document.querySelector(".progress");
-
+// Variables 
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
-
 let startID = document.getElementById("start");
 let breakID = document.getElementById("break");
 let resetID = document.getElementById("reset");
 
 // Initialization
-const initial = {
-    startTime: 25,
-    breakTime: 5,
+let initial = {
+    startTime: 24,
+    breakTime: 4,
     seconds: "00",
 }
 let timeContainer;
-
+let decrementSec = 59 
+// initial.seconds = 59;
 
 // Display on loading
 window.onload = () => {
-    min.innerHTML = initial.startTime;
+    min.innerHTML = initial.startTime + 1;
     sec.innerHTML = initial.seconds;
     breakID.style.display = "none";
 }
 
 // Start timer
 let start = () => {
+
     breakID.style.display = "block";
     startID.style.display = "none";
-    initial.seconds = 59;
     
+    // Set minutes to start count down
     let onWorking = initial.startTime - 1;       // 24  
     let onBreaking = initial.breakTime - 1;      // 4   
-    
-    breakCounter = 0;
 
-    // count down
+    breakCounter = 0;
+ 
+    //Add function count down
     let beginCountDown = () => {
-        // Display values in DOM once countdown starts
+
+        // Display values in DOM once countdown has start
         min.innerHTML = onWorking;
-        sec.innerHTML = initial.seconds;    /**
+        // sec.innerHTML = initial.seconds;   
+        sec.innerHTML = decrementSec;    
+        
+                                        /**
                                         * 24:59 
                                         */
 
-        // Start with seconds - 1
-        initial.seconds -= 1;          
+        // Decrement seconds  
+        decrementSec -=1      
 
-        if (initial.seconds === 0) {
+        // if seconds get value 0 then decrement minutes (onWorking)
+        if (decrementSec === 0) {
             onWorking -= 1;
 
             if (onWorking === -1) {
                 if (breakCounter % 2 === 0) {
-                    // Starts with breack timer
+                    // Start with breack timer
                     onWorking = initial.breakTime;
                     breakCounter+=1 
                 } else{
                     // Continue working
                     onBreaking = initial.startTime; //25
-                    breakCounter+=1
+                    breakCounter = 0;
                 }
             }
-            initial.seconds = 59;
+            decrementSec = 59;
         }       
-    }; timeContainer = setInterval(beginCountDown, 1000); // 1000ms
+    }; 
+    // set the interval to execute the function beginCountDown()
+    timeContainer = setInterval(beginCountDown, 1000); // 1000ms
 }
 
 // Add function to pause timer
 const breakSession = () => {
-    clearInterval(timeContainer);
-    // breakID.style.display = "none";
-    // startID.style.display = "block";
-    
+    clearInterval(timeContainer);    
 }
 
 // Add function to reset timer
@@ -96,9 +99,12 @@ startID.addEventListener('click', () => {
 // Add event listener to start button :
 breakID.addEventListener('click', () => {
     breakSession();
+    startID.style.display = "block";
+    breakID.style.display = "none";
 });
 
 // Add event listener to start button :
 resetID.addEventListener('click', () => {
     resetToDefault();
 });
+ 
